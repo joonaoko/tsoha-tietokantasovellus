0a8b806348719a8d5429489c9a957d1218251d4b
joonaoko <joonaoko@users.noreply.github.com>
2018-11-20 19:09:28 +0200

    Lisätty abstrakti Base-luokka ja alkeellinen yhteenveto-ominaisuus

--- application/series/models.py
+++ application/series/models.py
@@ -1,10 +1,10 @@
 from application import db
+from application.models import Base
 
-class Series(db.Model):
+from sqlalchemy.sql import text
+
+class Series(Base):
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     episodes_total = db.Column(db.Integer, nullable=False)
@@ -14,3 +14,14 @@ class Series(db.Model):
     def __init__(self, name):
         self.name = name
         self.episodes_total = 0;
+
+    @staticmethod
+    def find_series_with_no_episodes():
+        stmt = text("SELECT name FROM Series WHERE episodes_total = 0")
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"name":row[0]})
+        
+        return response
--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from application import app
+from application.series.models import Series
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", no_episodes=Series.find_series_with_no_episodes())
