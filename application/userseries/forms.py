1dde0741beb96cd13f594fd70f913e158cb5113f
joonaoko <joonaoko@users.noreply.github.com>
2018-12-05 14:04:07 +0200

    Lisätty sivu jonka kautta päästään muokkaamaan Watchlistin sarjojen statusta sekä katsottujen jaksojen määrää
    Edit-nappi list.html:ään
    Uusi update.html jossa muokkaus tehdään
    UserSeriesUpdateForm forms.py:hyn
    Tarvittavat userseries_update_form ja userseries_update lisätty views.py:hyn

--- application/userseries/forms.py
+++ application/userseries/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, validators
+from wtforms import IntegerField, StringField, validators, widgets
 
-class UserSeriesForm(FlaskForm):
-    episodes_watched = IntegerField("Episodes watched")
+class UserSeriesUpdateForm(FlaskForm):
+    episodes_watched = IntegerField("Episodes watched", 
+                                    [validators.NumberRange(min=0, max=99999)],
+                                    widget=widgets.Input(input_type="number"))
+    status = StringField("Status", [validators.Length(min=2, max=144)])
 
     class Meta:
         csrf = False
--- application/userseries/views.py
+++ application/userseries/views.py
@@ -3,7 +3,8 @@ from flask_login import login_required, current_user
 
 from application import app, db
 from application.userseries.models import UserSeries
-from application.userseries.forms import UserSeriesForm
+from application.series.models import Series
+from application.userseries.forms import UserSeriesUpdateForm
 
 from sqlalchemy.sql import text
 
@@ -11,6 +12,13 @@ from sqlalchemy.sql import text
 @login_required
 def userseries_index():
     return render_template("userseries/list.html", userseries = UserSeries.find_user_series())
+
+@app.route("/userseries/update/<userseries_id>/")
+@login_required
+def userseries_update_form(userseries_id):
+  us = UserSeries.query.get(userseries_id)
+  s = Series.query.get(us.series_id)
+  return render_template("userseries/update.html", userseries = us, series = s, form = UserSeriesUpdateForm())
 
 @app.route("/userseries/plus1/<userseries_id>/", methods=["POST"])
 @login_required
@@ -20,6 +28,21 @@ def userseries_increase_episodes_watched_by_one(userseries_id):
     db.session().commit()
 
     return redirect(url_for("userseries_index"))
+
+@app.route("/userseries/update/<userseries_id>/", methods=["POST"])
+@login_required
+def userseries_update(userseries_id):
+  form = UserSeriesUpdateForm(request.form)
+
+  if not form.validate():
+    return render_template("userseries/update.html", form = form)
+
+  us = UserSeries.query.get(userseries_id)
+  us.episodes_watched = form.episodes_watched.data
+  us.status = form.status.data
+  db.session().commit()
+
+  return redirect(url_for("userseries_index"))
 
 @app.route("/userseries/delete/<userseries_id>/", methods=["POST"])
 @login_required
