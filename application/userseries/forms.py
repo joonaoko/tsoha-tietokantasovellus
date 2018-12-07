8a8f7e587446a395fcab0893572cc3c21dbd509c
joonaoko <joonaoko@users.noreply.github.com>
2018-12-07 17:53:18 +0200

    Statuksen muokkaus muutettu ennaltamääritetyiksi valinnoiksi ja lomakkeen oletusarvoiksi laitettu vanhat arvot

--- application/userseries/forms.py
+++ application/userseries/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, validators, widgets
+from wtforms import IntegerField, SelectField, validators, widgets
 
 class UserSeriesUpdateForm(FlaskForm):
-    episodes_watched = IntegerField("Episodes watched", 
+    episodes_watched = IntegerField("Episodes watched",
                                     [validators.NumberRange(min=0, max=99999)],
                                     widget=widgets.Input(input_type="number"))
-    status = StringField("Status", [validators.Length(min=2, max=144)])
+    status = SelectField("Status", choices=[('Watching', 'Watching'), ('Maybe', 'Maybe')])
 
     class Meta:
         csrf = False
