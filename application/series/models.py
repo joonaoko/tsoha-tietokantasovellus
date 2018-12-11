202503762d88b59f994f45fc01fc5b82b64cbc24
joonaoko <joonaoko@users.noreply.github.com>
2018-12-11 23:48:32 +0200

    Lisätty viimepäivän aktiivisten sarjojen uniikkeus myös PostgreSQL-kyselyyn

--- application/series/models.py
+++ application/series/models.py
@@ -58,7 +58,7 @@ class Series(Base):
     @staticmethod
     def find_recently_watched_series():
         if os.environ.get("HEROKU"):
-            stmt = text("SELECT DISTINCT series.id, series.name AS name, user_series.date_modified "
+            stmt = text("SELECT DISTINCT ON (series.id) series.id, series.name AS name, user_series.date_modified "
                         "FROM user_series INNER JOIN series ON user_series.series_id = series.id "
                         "WHERE user_series.date_modified >= now() - interval '24 hour' "
                         "ORDER BY user_series.date_modified DESC LIMIT 10")
